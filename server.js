4d90b1521c87befa57036fa306670dacd9753760
Kevinjj30 <kevinjenkins56@yahoo.com>
2020-05-16 10:40:27 -0500

    deploy14

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(require("webpack-hot-middleware")(compiler));
 
 app.use(function(req, res, next) {
     if (req.headers['x-forwarded-proto'] === 'https') {
-        res.redirect('https://' + req.hostname + req.url);
+        res.redirect('http://' + req.hostname + req.url);
     } else {
         next();
     }
