c5658da94054ece0872a74568d03a7a8efbbfd37
Ubuntu <ubuntu@ip-172-31-17-221.us-east-2.compute.internal>
2020-05-17 18:41:54 +0000

    config changes

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,16 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: 'root',
-    database: 'Techy1',
-    host: '127.0.0.1',
+    username: 'techyecom',
+    password: 'techy123#',
+    database: 'techyecomm',
+    host: 'database-1.c9kpucnskl1o.us-east-2.rds.amazonaws.com',
     dialect: 'mysql'
   },
   test: {
-    username: 'root',
-    password: 'root',
-    database: 'boilerplate_test',
-    host: '127.0.0.1',
+    username: 'techyecom',
+    password: 'techy123#',
+    database: 'techyecomm',
+    host: 'database-1.c9kpucnskl1o.us-east-2.rds.amazonaws.com',
     dialect: 'mysql',
     dialectModule: 'mysql2',
     logging: null
